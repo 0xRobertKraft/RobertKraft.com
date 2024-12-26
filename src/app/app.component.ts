484abc67d86b750baa111344ee2e6301242abeaf
RobertKraft <royhoffmantopdev@gmail.com>
2024-12-26 04:25:20 -0800

    updated project px perfecr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./header/header.component";
 import { ContentComponent } from "./main/content.component";
 import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, ContentComponent, FooterComponent],
+  imports: [RouterOutlet, ContentComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
